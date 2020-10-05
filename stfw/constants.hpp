2bf6c7b83981adea745e183ab5c5ab10011f51cf
natfaulk <nfaulk@hotmail.co.uk>
2020-10-05 15:59:30 +1300

    added changelog

--- stfw/constants.hpp
+++ stfw/constants.hpp
@@ -4,4 +4,18 @@
 static const char*    VERSION                                = "1.0.0";
 static const bool     START_BY_DEFAULT                       = false;
 
+/* Changelog
+ *
+ * v1.0.0
+ *  - Initial release. Data sent over serial as JSON
+ *  - Can control with the following serial commands:
+ *      - r : reset
+ *      - u : get UID
+ *      - i : get board type string (thermo board)
+ *      - v : get FW version
+ *      - s : start running
+ *      - e : end running
+ * 
+ */
+
 #endif
