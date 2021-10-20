a8c3cedc8eb40ccd7f44155b21331dc7612f6b09
natfaulk <nfaulk@hotmail.co.uk>
2021-10-20 15:15:05 +0100

    remove old code

--- displayapp/src/utils.js
+++
@@ -1,7 +0,0 @@
-export default function checkKeyHasVal(_obj, _key) {
-  return (
-    _key in _obj 
-    && _obj[_key] !== null
-    && _obj[_key] !== undefined
-  )
-}
--- thermo_fw/network.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef NETWORK_HPP
-#define NETWORK_HPP
-
-#include <stdint.h>
-
-void NET_Init(void);
-bool NET_SendData(const float* _data, uint16_t _len);
-
-#endif
